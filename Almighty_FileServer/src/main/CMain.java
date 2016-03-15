b2a9176c3c96a61020d4be83625e78095b16ebbb
#CHO DA YANG BRYDEN# <BCHO002@student.main.ntu.edu.sg>
2016-03-15 14:38:21 +0800

    Create Project

---
+++ Almighty_FileServer/src/main/CMain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author bcho002
+ */
+public class CMain {
+
+    public static void main(String... pAryArgs) {
+
+        System.out.println("Hello Dr Tang and Dr Sun");
+        
+    }
+
+}
