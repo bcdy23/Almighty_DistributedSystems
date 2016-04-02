2d8e544fc97e7b2d1160498e7ee35ac8f1ecd3b8
almightyGOSU <gosujinyao@gmail.com>
2016-04-02 11:21:40 +0800

    Commit cuz King Chody asked me to do so

--- Almighty_FileServer/src/comm/CServerManager.java
+++ Almighty_FileServer/src/comm/CServerManager.java
@@ -11,12 +11,35 @@ public class CServerManager {
 
     public static String performOperation(byte[] pAryData) {
 
-        
+        int offset = 0;
+    	
         // Need to provide start offset
-        ECommand objCommand = ECommand.getCommand(unmarshallInt(pAryData));
-
+        ECommand objCommand = ECommand.getCommand(unmarshallInt(pAryData, 0));
+        offset += 4;
         
-        String strPathName = unmarshallString(pAryData).toString();
+        switch(objCommand) {
+		case ACK:
+			break;
+		case CREATE:
+			break;
+		case DELETE:
+			break;
+		case ERROR:
+			break;
+		case MONITOR:
+			break;
+		case MOVE:
+			break;
+		case READ:
+			break;
+		case UPDATE:
+			break;
+		case WRITE:
+			break;
+		default:
+			break;
+        }
+        String strPathName = unmarshallString(pAryData, offset).toString();
         
         return "";
     }
